2e2ab78ed8133d4fd7f5559d7f7f2b30c1e375b7
Jay Greco <jayv.greco@gmail.com>
2022-03-03 22:58:26 -0800

    Add SNAP

---
+++ config.h
@@ -0,0 +1,82 @@
+/* Copyright 2021 Jay Greco
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+// clang-format off
+#pragma once
+
+#include "config_common.h"
+
+/* Used to set host for remote KB if VUSB detect doesn't work. */
+// #define KEYBOARD_HOST // Force host mode
+// #define KEYBOARD_REMOTE // Force remote mode
+
+// Workaround for freezing after MacOS sleep
+#define USB_SUSPEND_WAKEUP_DELAY 200
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0x6E61
+#define PRODUCT_ID      0x6063
+
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    nullbits
+#define PRODUCT         SNAP
+
+#define SOFT_SERIAL_PIN E6
+#define SPLIT_HAND_PIN B6
+#define DISABLE_SYNC_TIMER
+#define SPLIT_HAND_PIN_LOW_IS_LEFT
+
+/* key matrix size */
+#define MATRIX_ROWS 12
+#define MATRIX_COLS 9
+#define MATRIX_MUX_COLS 3
+
+/* Set 0 if debouncing isn't needed */
+#define DEBOUNCE 10
+
+/*
+ * Keyboard Matrix Assignments
+ * The snap uses a demultiplexer for the cols.
+ * to free up more IOs for awesomeness!
+ * See matrix.c for more details.
+*/
+// Left side
+#define MATRIX_ROW_PINS { D4, C6, D7, F4, B4, B5 }
+#define MATRIX_COL_MUX_PINS { F7, F6, F5 }
+
+//Right side
+#define MATRIX_ROW_PINS_RIGHT { F4, F5, F6, F7, B1, B3 }
+#define MATRIX_COL_MUX_PINS_RIGHT { D7, C6, D4 }
+#define MATRIX_EXT_PIN_RIGHT B6
+
+/* Optional SMT LED pins */
+#define RGB_DI_PIN B2
+#define RGBLED_NUM 10
+#define RGBLED_SPLIT { 5, 5 }
+#define RGBLIGHT_LED_MAP { 8, 9, 0, 1, 2, 6, 7, 3, 4, 5 }  
+#define RGBLIGHT_ANIMATIONS
+#define RGBLIGHT_SLEEP
+
+/* Optional encoder pins */
+#define ENCODERS_PAD_A { B3 }
+#define ENCODERS_PAD_B { B1 }
+#define ENCODERS_PAD_A_RIGHT { B4 }
+#define ENCODERS_PAD_B_RIGHT { B5 }
+
+/* Optional speaker pin */
+#define AUDIO_PIN B6
+
+/* Optional split transactions */
+#define SPLIT_LED_STATE_ENABLE
---
+++ rules.mk
@@ -0,0 +1,33 @@
+# MCU name
+MCU = atmega32u4
+
+# Bootloader selection
+BOOTLOADER = atmel-dfu
+
+# Build Options
+#   change yes to no to disable
+#
+BOOTMAGIC_ENABLE = no      # Virtual DIP switch configuration
+MOUSEKEY_ENABLE = no       # Mouse keys
+EXTRAKEY_ENABLE = yes      # Audio control and System control
+CONSOLE_ENABLE = no        # Console for debug
+COMMAND_ENABLE = no        # Commands for debug and configuration
+SLEEP_LED_ENABLE = no      # Breathing sleep LED during USB suspend
+NKRO_ENABLE = no           # USB Nkey Rollover
+BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality on B7 by default
+MIDI_ENABLE = no           # MIDI support
+UNICODE_ENABLE = no        # Unicode
+BLUETOOTH_ENABLE = no      # Enable Bluetooth with the Adafruit EZ-Key HID
+AUDIO_ENABLE = no          # Audio output
+SPLIT_KEYBOARD = yes       # Split common
+LTO_ENABLE = yes           # Use Link Time Optimization
+OLED_DRIVER_ENABLE = no    # Enables the use of OLED displays
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+CUSTOM_MATRIX = lite       # Split custom matrix
+
+# Project specific files
+SRC +=  common/bitc_led.c \
+        common/remote_kb.c \
+        matrix.c \
+        uart.c
