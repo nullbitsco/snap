69df0896522a6d6965d088bb056dcd7c1acf957c
Jay Greco <jayv.greco@gmail.com>
2022-05-09 17:57:04 +0000

    Change OLED update rate to support remote KB

--- keymaps/typehud/config.h
+++ keymaps/typehud/config.h
@@ -19,6 +19,7 @@
 // Old configuration
 #define OLED_BRIGHTNESS 128
 #define OLED_TIMEOUT 30000
+#define OLED_UPDATE_INTERVAL 200
 
 // Selectively define RGB modes to save some space
 // VIA support won't fit otherwise
